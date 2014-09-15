dfca2002022dea5ef5d9894f29222e5d66b8a441
cobi zaltzberg <jzaltzberg@gmail.com>
2014-09-15 22:11:42 +0300

    host config change

--- config/Config.host.php
+++ config/Config.host.php
@@ -6,10 +6,10 @@ define('APP_BASE', '/agenda/');
 
 define('DB_PREFIX', '');
 
-define('DB_HOST', 'localhost');
+define('DB_HOST', 'web-dev');
 
-define('DB_USER', 'root');
+define('DB_USER', 'ravtech');
 
-define('DB_PASS', '');
+define('DB_PASS', 'jf26su8w');
 
 define('DB_NAME', 'agenda');
